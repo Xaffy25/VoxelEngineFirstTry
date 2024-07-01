420031627456630523a9bdf4c111d8374aba40bc
Sniegawa <xaffy25@gmail.com>
2024-07-01 17:23:58 +0200

    First window!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
-#include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm.hpp>
+using namespace glm;
 int main()
 {
-	std::cout << "Hello World";
+	glewExperimental = true;
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+	glfwWindowHint(GLFW_SAMPLES, 4);//Anti aliasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1024, 768, "VOXEL ENGINE", NULL, NULL);
+	if (window == NULL)
+	{
+		fprintf(stderr, "Failed to create window");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) 
+	{
+		fprintf(stderr, "Failed to initialize GLEW");
+		return -1;
+	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do 
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+	
+
 	return 0;
 }
